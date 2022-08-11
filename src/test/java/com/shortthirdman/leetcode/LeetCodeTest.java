ed1e6ee718821994addf2ce7a0cc5acd92ee7ce9
Swetank Mohanty <shortthirdman@users.noreply.github.com>
2022-08-11 19:08:44 +0530

    Added test cases for MaxChunksForSortedArray and configured Log4J 2

--- src/test/java/com/shortthirdman/leetcode/LeetCodeTest.java
+++ src/test/java/com/shortthirdman/leetcode/LeetCodeTest.java
@@ -1,5 +1,7 @@
 package com.shortthirdman.leetcode;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -8,18 +10,34 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 public class LeetCodeTest {
 
+    private static final Logger logger = LogManager.getLogger(LeetCodeTest.class);
+
     @Test
     public void kthSmallestElementMatrix() {
+        logger.info("Running test case for kth Smallest Element in a Matrix");
         int[][] inputMatrix = {
                 {1, 5, 9},
                 {10, 11, 13},
                 {12, 13, 15}
         };
         int k = 8;
-        int result = 13;
+        int expected = 13;
 
         KthSmallestElementMatrix ref = new KthSmallestElementMatrix();
+        int result = ref.kthSmallest(inputMatrix, k);
 
-        assertThat(ref.kthSmallest(inputMatrix, k)).isEqualTo(result);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void maxChunksToSortArray() {
+        logger.info("Running test case for Maximum chunks to make Sorted Array");
+        int arr[] = { 2, 0, 1 };
+        int expected = 1;
+
+        MaxChunksForSortedArray ref = new MaxChunksForSortedArray();
+        int result = ref.maxChunksToSortArray(arr);
+
+        assertThat(result).isEqualTo(expected);
     }
 }
